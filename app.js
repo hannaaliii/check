78e0a0217d290a22f0b428c9249fdd2507c24f71
hannaaliii <hannaali.tt@gmail.com>
2020-06-29 13:04:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// function greetings(name){
+//     console.log(`hello ${name}. welcome to node.js`);
+// }
+// greetings("Hanna");
+
+// console.log(`Process Platform:${process.platform}`);
+// console.log(`Process Platform:${process.version}`);
+// console.log(`working directory:${process.cwd()}`);
+
+
+console.log("Gretings"); //Global Scope
+// window.console.log("Browser"); //Browser Environment//unkown to nodejs
+global.console.log("Node"); //Global Scope
+ var name = "hanna";
+ console.log(name);
---
+++ messenger.js
@@ -0,0 +1,6 @@
+function greeter(name){
+    console.log(`hello ${name}`);
+}
+// greeter("hanna");
+
+module.exports.printer=greeter;
